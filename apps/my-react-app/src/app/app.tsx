a54cf2224ecd812d0ae9127abb4e17e68a270b5e
tergeltengis <tergeltengis77@gmail.com>
2022-11-28 15:15:45 +0800

    initial commit

--- apps/my-react-app/src/app/app.tsx
+++ apps/my-react-app/src/app/app.tsx
@@ -1,10 +1,23 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import { useEffect, useState } from 'react';
 import NxWelcome from './nx-welcome';
+import { ApiResponse, API_URL } from '@nx-test/api-interface';
 
 export function App() {
+  const [message, setMessage] = useState<ApiResponse>({
+    message: 'LOADING...',
+  });
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(API_URL);
+      const data = await res.json();
+      setMessage(data);
+    })();
+  }, []);
   return (
     <>
+      <h1>{message.message}</h1>
       <NxWelcome title="my-react-app" />
       <div />
     </>
